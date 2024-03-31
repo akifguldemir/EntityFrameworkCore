1f322550746ba493d5ce61d213b8fd19e181c9d5
akifguldemir <akifguldemir@bontesoft.com>
2024-03-31 23:30:14 +0300

    Create completed

---
+++ Data/StudentController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkCore.Data
+{
+    public class StudentController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(DataContext context, ILogger<StudentController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Add(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Privacy","Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+    }
+}
