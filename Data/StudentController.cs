2d23ee090654283b948221921588e321f22f5f0f
akifguldemir <akifguldemir@bontesoft.com>
2024-04-01 00:49:55 +0300

    Edit Completed

--- Data/StudentController.cs
+++ Data/StudentController.cs
@@ -42,10 +42,41 @@ namespace EntityFrameworkCore.Data
         {
             if(id == null) return NotFound();
 
-            var student = await _context.Students.FindAsync(id);
+            // var student = await _context.Students.FindAsync(id);
+            var student = await _context.Students.FirstOrDefaultAsync(s => s.Id == id);
             if(student == null) return NotFound();
 
             return View(student);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, Student model)
+        {
+            if(id != model.Id) return NotFound();
+
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Students.Any(s => s.Id == model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
