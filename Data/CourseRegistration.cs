1a1b9f6c52fcba35d0226d9590ceaa2e1c662f54
akifguldemir <akifguldemir@bontesoft.com>
2024-03-31 21:57:03 +0300

    props fixed

--- Data/CourseRegistration.cs
+++ Data/CourseRegistration.cs
@@ -8,8 +8,8 @@ namespace EntityFrameworkCore.Data
     public class CourseRegistration
     {
         public int Id { get; set; }
-        public int OgrenciId { get; set; }
-        public int KursId { get; set; }
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
         public DateTime RegisterDate { get; set; }
     
     }
